d11f0f6bd848b7aa46b69394d03cad00685fa339
astand02 <135463433+astand02@users.noreply.github.com>
2023-09-27 15:55:11 -0500

    added schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Enginering"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ( "Sales Lead", 100000, 1),
+       ( "Salesperson", 80000, 1),
+       ( "Lead Engineer", 150000, 2),
+       ( "Software Engineer", 120000, 2),
+       ( "Account Manager", 160000, 3),
+       ( "Accountant", 125000, 3),
+       ( "Legal Team Lead", 250000, 4),
+       ( "Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ( "John", "Doe", 1, NULL),
+       ( "Mike", "Chan", 1, 1),
+       ( "Ashley", "Rodriguez", 2, ),
+       ( "Kevin", "Tupik", 2),
+       ( "Kunal", "Singh", 3),
+       ( "Malia", "Brown", 3),
+       ( "Sarah", "Lourd", 4),
+       ( "Tom", "Allen", 4);
+
+
+       
